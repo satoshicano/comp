3f6e8cebcc1b91bd1b384994e9187eaf83d5c508
m-kawabe <g2116012@fun.ac.jp>
2017-01-15 12:45:52 +0900

    add GET /recipes/1 json api

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,35 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1
+  # {“title”: “さとしのカレー”, “servings”: 2, ”foods”: [{ {“name”:”じゃがいも”, “nutrients”: {...}} , “num”:1 } ],
+  #  “steps”: [{ "order": 1,  “foods”: [1,2,3], “step_id”: 1}, {"order": 2, “foods”: [1,2,3], “step_id”: 2}] }
+
   def show
-    render json: @recipe
+    foods = @recipe.foods.map do |f|
+      {
+        id: f.id,
+        title: f.name,
+        option: f.option,
+        leather: f.leather,
+        price: f.cost,
+        num: FoodRecipe.includes(:recipe).find_by({food_id: f.id, recipe_id: @recipe.id }).number,
+        nutrients: {
+          calorie: f.calorie,
+          ash: f.ash,
+          cholesterol: f.cholesterol,
+          protein: f.protein,
+          water: f.water,
+          fiber: f.fiber,
+          carb: f.carb
+        }
+      }
+    end
+
+    steps = @recipe.food_steps.joins(:step, :food).includes(:step, :food).group_by {|fs| fs.step.name}.map do |name, fss| {
+      title: name, foods: fss.map{|fs| fs.food.name}, time: fss.first.step.time }
+    end
+
+    render json: {title: @recipe.title, servings: @recipe.servings, foods: foods, steps: steps}
   end
 
   # POST /recipes
